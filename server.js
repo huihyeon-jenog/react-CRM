0af25d95b3195c6fe8d9a246ad939c4b08b6a7dd
huihyeon-jenog <wjdgmlgus5@naver.com>
2021-02-25 01:28:17 +0900

    node.js Mysql Connection

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const fs = require('fs')
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+const data = fs.readFileSync('./database.json')
+const conf = JSON.parse(data)
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+})
+connection.connect();
+app.get('/api/customers',(req,res)=>{
+    connection.query(
+        'SELECT * FROM CUSTOMER',
+        (err,rows,fields)=>{
+            res.send(rows)
+        }
+    )
+});
+app.listen(port,()=>console.log(`Listening on port ${port}`))
